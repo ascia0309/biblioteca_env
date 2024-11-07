2a1cf9ed3b92b348a09079a2901aea97723fd61f
C. Isaac Diaz <cesariac.diaz0309@gmail.com>
2024-11-07 15:20:06 -0500

    eliminado y la nueva version

--- Biblioteca/Bibliote_Server/ve_bibliote/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\proyectoati\biblioteca\bibliote_server\ve_bibliote\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
